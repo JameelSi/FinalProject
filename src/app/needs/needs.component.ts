7e14cb7446d68370ce334accf5edf62e82b90cb1
JameelSi <silwadij@gmail.com>
2021-05-15 02:41:01 +0300

    updated answers

--- src/app/needs/needs.component.ts
+++ src/app/needs/needs.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject,OnInit,ViewChild,OnDestroy } from '@angular/core';
+import { Component,OnInit,ViewChild,OnDestroy } from '@angular/core';
 import { MatDrawer } from '@angular/material/sidenav';
 import { BreakpointObserver } from '@angular/cdk/layout';
 import {MatDialog} from '@angular/material/dialog';
@@ -18,7 +18,6 @@ export class NeedsComponent implements OnInit,OnDestroy {
 
   @ViewChild(MatDrawer)
   sidenav!: MatDrawer;
-  direction =["to top","to top right","to right","to bottom right","to bottom","to bottom left","to left","to top left"];
   arr: Array<String>=[];
   name: string="";
   config!: MatSnackBarConfig
@@ -29,30 +28,15 @@ export class NeedsComponent implements OnInit,OnDestroy {
   private itemsCollection: AngularFirestoreCollection<Item>;
   items!: Observable<Item[]>;
 
-    setCurrentStyles() {
-      // CSS styles: set per current state of component properties
-      this.currentStyles = {
-        'background':  this.backgroundCol
-      };
-    }
- 
   constructor(private observer: BreakpointObserver,public dialog: MatDialog,readonly snackBar: MatSnackBar, private afs: AngularFirestore) {
     this.itemsCollection = afs.collection<Item>('items');
   }
   
   ngOnInit(){
-    // this.itemsCollection=this.afs.collection('Services', ref =>{
-    //   return ref.orderBy('sth')
-    // })
     this.itemsCollection=this.afs.collection('Services', ref =>{
         return ref.orderBy("title")
       })
     this.items=this.itemsCollection.valueChanges({ idField: 'id' });
-
-    // this.items.subscribe(val => {
-    //   for(let i=0;i<val.length;i++)
-    //     this.arr.push(val[i].title)
-    // })
   }
 
   ngAfterViewInit() {
@@ -70,14 +54,16 @@ export class NeedsComponent implements OnInit,OnDestroy {
   });
   }
 
-  addDialog(action: 'Update' | 'Delete' | 'Add', element: any,type:'doc'|'collection'): void {
+  addDialog(action: 'Update' | 'Delete' | 'Add', element: any,type:'doc'|'collection',answerContent?:string): void {
+      
       if(action==='Add' &&type=='collection')
         element.dialogTitle='שם של הצורך שברצונך להוסיף?'
       else if(action==='Delete' &&type=='collection')
         element.dialogTitle='בטוח למחוק את הצורך וכל מעניו?'
+
       else if(action==='Add' &&type=='doc')
         element.dialogTitle='נא להקליד את תוכן המענה'
-      else if(action==='Update' &&type=='doc')
+      else if(action==='Delete' &&type=='doc')
         element.dialogTitle='בטוח למחוק ?'
       else if(action==='Update')
         element.dialogTitle='מה הערך החדש?'
@@ -88,11 +74,14 @@ export class NeedsComponent implements OnInit,OnDestroy {
         direction:'rtl',
         data:element,
       });
-  
+      
       dialogRef.afterClosed().subscribe(result => {
         if(result){
           if(result.event == 'Add' && type=='collection'){
-            this.afs.collection('Services').add({title:result.data.name,content:[]})
+            if(true)
+              this.snackBar.open("הצורך כבר נמצא !",'',{duration:1500,direction:'rtl',panelClass:['snacks']});
+            else
+              this.afs.collection('Services').add({title:result.data.name,content:[]})
           }else if(result.event == 'Delete' && type=='collection'){
             this.afs.collection('Services').doc(result.data.id).delete()
           }else if(result.event =='Update' && type=='collection'){
@@ -100,12 +89,18 @@ export class NeedsComponent implements OnInit,OnDestroy {
           }
           
           else if(result.event =='Add' && type=='doc'){
-            result.data.content.push(result.data.name)
-            this.afs.doc(`Services/${result.data.id}`).update({content:result.data.content})
+            if(result.data.content.indexOf(result.data.name)!=-1)
+              this.snackBar.open("הצורך כבר נמצא !",'',{duration:1500,direction:'rtl',panelClass:['snacks']});
+            else{
+              result.data.content.push(result.data.name)
+              this.afs.doc(`Services/${result.data.id}`).update({content:result.data.content})
+            }
           }else if(result.event =='Delete' && type=='doc'){
-            console.log(result.data.name)
+            result.data.content.splice(result.data.content.indexOf(answerContent),1)
+            this.afs.doc(`Services/${result.data.id}`).update({content:result.data.content})
           }else if(result.event =='Update' && type=='doc'){
-            console.log(result.data.id)
+            result.data.content[result.data.content.indexOf(answerContent)]=result.data.name;
+            this.afs.doc(`Services/${result.data.id}`).update({content:result.data.content})
           }
         }
           
@@ -113,11 +108,6 @@ export class NeedsComponent implements OnInit,OnDestroy {
   
     
   }
-
-  addContent( id:string, content:Array<string>): void {
-    content.push('hellos')
-      this.afs.doc(`Services/${id}`).update({content:content})
-    }
 
     ngOnDestroy(): void {
       this.subs.unsubscribe();
